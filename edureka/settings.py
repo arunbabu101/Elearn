06b4065a53ad2be26918902f42632d8448e8f552
Arun Babu <arunbabu43916@gmail.com>
2024-08-03 16:57:19 +0530

    new

--- edureka/settings.py
+++ edureka/settings.py
@@ -89,10 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'NAME': 'edureka',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'Shivam123',
-        # 'HOST': 'edurekanet.herokuapp.com',
+    
     }
 }
 
